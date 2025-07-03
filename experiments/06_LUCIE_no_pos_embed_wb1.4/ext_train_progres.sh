497b2b146f03e78bb216eb1da870a00ecd27e31e
Yue Sun <yxs900@gadi-login-01.gadi.nci.org.au>
2025-07-03 15:13:24 +1000

    small correction

--- experiments/06_LUCIE_no_pos_embed_wb1.4/ext_train_progres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-jobid=$1
-inp=$2
-src=${inp}.sh.o${jobid}
-grep "clim_bias: " ${src} | awk '{print $2}' > clim.bias.o${jobid}.csv
-grep loss ${src} | awk '{print $4}' > average.training.loss.o${jobid}.csv
